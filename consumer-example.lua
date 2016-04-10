d423a5f0df5e8efb810b14925312803e3844bd6d
Brandon <lordnynex@gmail.com>
2016-04-10 14:07:14 -0700

    Squashed 'FORKS/Lua/rabbitmq-lua/tree/' content from commit 5313b92
    git-subtree-dir: FORKS/Lua/rabbitmq-lua/tree
    git-subtree-split: 5313b9220f43c53874fd423c2a5a962a929e104b

---
+++ consumer-example.lua
@@ -0,0 +1,17 @@
+rb=require "amqp-util"
+
+conn = rb.connect_rabbit{host="localhost"}
+rb.declare_exchange(conn, "LuaExchange", "fanout")
+rb.declare_queue(conn,"LuaQueue")
+rb.declare_queue(conn,"LuaQueue2")
+rb.bind_queue(conn, "LuaQueue","LuaExchange")
+rb.bind_queue(conn, "LuaQueue2","LuaExchange")
+
+function qprint(consumer_tag,data)
+   io.write(string.format("tag=%s, data=%s\n",consumer_tag,data))
+end
+
+rb.wait_for_messages(conn, { LuaQueue = {qprint, {no_ack=1}},
+			     LuaQueue2 = {qprint} })
+			
+
