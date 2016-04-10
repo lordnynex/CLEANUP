2bde094a45ecde7c2715b5dc578d64b466a32261
Brandon <lordnynex@gmail.com>
2016-04-09 19:00:19 -0700

    An initial savepoint before I start merging sub trees

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # CLEANUP
+
+I have this really annoying habit of forking repositories I like or want to experiment with instead of simply staring them. After awhile I end up with repositories everywhere and it's bugged me for long enough.
+
+## Why?
+
+Several years ago I made the mistake of writing some code that depended on someones experimental library. All worked well until the next day I went to work and attempted to pickup where I left off and the owner of this lib's repo deleted it because he believed it was 'bad work'. For convoluted reasons, I did not have a copy of the lib anymore. Even if I had a copy of it somewhere, it made me realize the ephemeral nature of peoples personal projects. Something they see as a temporary experiment with a short shelf life can sometimes serve as inspiration or reference for unrelated projects for years to come. I use these forks almost daily. They serve as architectural inspiration, points of reference, or simply a way for me to freeze something in time.
+
+My hope is is that I will be able to use these things more effectively. I have a tendency sometimes to recall I have a fork that solves a similar problem, but absolutely can not remember which fork it was. At least now they'll be consolidated and I can use code searching tools to help.
---
+++ bin/CLEANUP.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+set -e
+
+# A hacky script to cleanup my github account
+#  - brandon beveridge
+
+# Find the jq binary.
+JQ=$(command -v jq 2>/dev/null)
+
+# Find the curl binary.
+CURL=$(command -v curl 2>/dev/null)
+
+# Find the git binary.
+GIT=$(command -v git 2>/dev/null)
+
+# Abs path
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Root dir
+ROOT_DIR=$($GIT rev-parse --show-toplevel)
+
+# Github username
+GH_USERNAME=lordnynex
+
+# Avoid bashing the github API multiple times
+# Store the output in a temp file so we can
+# make multiple passes
+GH_REPOS_TMP=$(mktemp)
+GH_FORKS_TMP=$(mktemp -d)
+
+# Misc vars
+FORK_DIR_NAME=FORKS
+
+# Runtime vars
+FORKS_DIR="${ROOT_DIR}/${FORK_DIR_NAME}"
+
+# Define a trap to remove this temp file on
+# exit.
+trap "rm -rf ${GH_REPOS_TMP} ${GH_FORKS_TMP}" EXIT
+
+# Fetch my public repos
+function fetch_repos() {
+  # type=source includes forks for some reason. I guess this only works for orgs
+  $CURL -s https://api.github.com/users/${GH_USERNAME}/repos -o $GH_REPOS_TMP
+}
+
+# Find repos that are forks and collapse them into this repo as submodules
+function collapse_forks() {
+  cd $ROOT_DIR
+
+  # Create forks dir if it does not exist
+  [ ! -d "${FORKS_DIR}" ] && mkdir -p ${FORKS_DIR}
+
+  for REPO_NAME in `cat $GH_REPOS_TMP | jq -r -c '.[] | select(.fork == true) | .name'`; do
+    obj=${GH_FORKS_TMP}/$REPO_NAME.json
+    cat $GH_REPOS_TMP | jq -r -c --arg repo $REPO_NAME '.[] | select(.fork == true) | select(.name == $repo)' > ${obj}
+
+    len=$(wc -l $obj)
+    if [[ $len == 0 ]]; then
+      exit
+    fi
+
+    FORK_LANG=$(cat $obj | jq -r '.language')
+    REPO_URL=$(cat $obj | jq -r '.url')
+
+    if [ "${FORK_LANG}" == "null" ]; then
+      FORK_LANG="Misc"
+    fi
+
+    DIR_LANG="${FORKS_DIR}/${FORK_LANG}"
+    DIR_REPO_TREE="${DIR_LANG}/${REPO_NAME}"
+
+    # Create LANG dir if it doesn't exist
+    [ ! -d "${DIR_LANG}" ] && mkdir -p ${DIR_LANG}
+
+    # Create repo dir if it doesn't exist
+    [ ! -d "${DIR_REPO_TREE}" ] && mkdir -p ${DIR_REPO_TREE}
+
+    # Save the listing for debugging
+    cp $obj ${DIR_REPO_TREE}/repo_listing.json
+
+    # Fetch repo info which contains parent info
+    # Be nice to the github API
+    if [ ! -f "${DIR_REPO_TREE}/repo_detail.json" ]; then
+      $CURL -s $REPO_URL -o ${DIR_REPO_TREE}/repo_detail.json
+    fi
+  done
+
+  # Fill empty directories with .gitkeep files to avoid ugly web listings
+  find ${FORKS_DIR} -maxdepth 1 -type d '!' -exec test -e "{}/.gitkeep" ';' -exec touch "{}/.gitkeep" ';'
+
+  # Walk for any new additions
+  for repo in `find ./${FORK_DIR_NAME} -mindepth 2 -maxdepth 2 -type d '!' -exec test -d '{}/tree/' ';' -print`; do
+    # Extract original parent clone url
+    PARENT_CLONE_URL=$(cat ${repo}/repo_detail.json | jq -r '.parent.clone_url')
+
+    # Add & Commit before merging the subtree
+    $GIT add $repo/
+    $GIT commit -m '[COLLAPSE] Add fork to tree.'
+    $GIT subtree add --prefix ${repo}/tree ${PARENT_CLONE_URL} master -squash
+  done
+}
+
+fetch_repos
+collapse_forks
